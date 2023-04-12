5688ef5bf5d05794ada932597cbff467ef53819c
Chris Chuck <chrischuckgithub@gmail.com>
2023-04-12 17:01:34 -0500

    naming

--- client/src/components/cell.js
+++ client/src/components/cell.js
@@ -1,62 +1,76 @@
-import React from 'react'
+import React from "react";
 
 export default class Cell extends React.Component {
-
   renderIcon() {
-    const { fill } = this.props
+    const { fill } = this.props;
     if (!fill) {
-      return null
+      return null;
     }
 
-    if (fill === 'HOOMAN') {
+    if (fill === "HOOMAN") {
       return (
         // Thanks w3schools! https://www.w3schools.com/graphics/svg_line.asp
         <svg height="50" width="50">
-          <line x1="0" y1="0" x2="50" y2="50" style={{stroke:'black', strokeWidth:3}} />
-          <line x1="0" y1="50" x2="50" y2="0" style={{stroke:'black', strokeWidth:3}} />
+          <line
+            x1="0"
+            y1="0"
+            x2="50"
+            y2="50"
+            style={{ stroke: "black", strokeWidth: 3 }}
+          />
+          <line
+            x1="0"
+            y1="50"
+            x2="50"
+            y2="0"
+            style={{ stroke: "black", strokeWidth: 3 }}
+          />
         </svg>
-      )
+      );
     }
 
-    if (fill === 'COMPUTER') {
+    if (fill === "COMPUTER") {
       return (
         // Thanks again w3schools! https://www.w3schools.com/graphics/svg_circle.asp
         <svg height="100" width="100">
-          <circle cx="50" cy="50" r="40" style={{stroke:'black', strokeWidth:3, fill: 'white' }} />
+          <circle
+            cx="50"
+            cy="50"
+            r="40"
+            style={{ stroke: "black", strokeWidth: 3, fill: "white" }}
+          />
         </svg>
-      )
+      );
     }
   }
 
   clickCell = () => {
-    const { cell, fillCell, fill, turn, isGameOver } = this.props
-    if (fill || turn !== 'HOOMAN' || isGameOver) {
-      return
+    const { cell, fillCell, fill, turn, isGameOver } = this.props;
+    if (fill || turn !== "HOOMAN" || isGameOver) {
+      return;
     }
-    fillCell(cell, 'HOOMAN')
-  }
+    fillCell(cell, "HOOMAN");
+  };
 
   render() {
-    const { cell, gridSize, fill, isGameOver, } = this.props
-    const [row, column] = cell
+    const { cell, gridSize, fill, isGameOver } = this.props;
+    const [row, column] = cell;
 
     return (
-      <div 
+      <div
         onClick={this.clickCell}
         style={{
-          width: '100px',
-          height: '100px',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRight: column < gridSize - 1 ? '1px solid red' : 'none',
-          cursor: !fill && !isGameOver ? 'pointer' : 'default'
+          width: "100px",
+          height: "100px",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          borderRight: column < gridSize - 1 ? "1px solid red" : "none",
+          cursor: !fill && !isGameOver ? "pointer" : "default",
         }}
       >
-        {
-          this.renderIcon()
-        }
+        {this.renderIcon()}
       </div>
-    )
+    );
   }
 }
--- client/src/components/grid.js
+++ client/src/components/grid.js
@@ -1,167 +1,190 @@
-import React from 'react'
+import React from "react";
 
-import Cell from './cell'
+import Cell from "./cell";
 
-const DEFAULT_GRID_SIZE = 3
-const DEFAULT_TURN = 'HOOMAN' 
+const DEFAULT_GRID_SIZE = 3;
+const DEFAULT_TURN = "HOOMAN";
 
 const initGrid = (size) => {
-  const grid = []
-  const gridSize = size || DEFAULT_GRID_SIZE
+  const grid = [];
+  const gridSize = size || DEFAULT_GRID_SIZE;
 
   for (let i = 0; i < gridSize; i++) {
-    const row = []
+    const row = [];
     for (let j = 0; j < gridSize; j++) {
-      row.push('')
+      row.push("");
     }
-    grid.push(row)
+    grid.push(row);
   }
-  return grid
-}
-
+  return grid;
+};
 
 export default class Grid extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
     this.state = {
       isGameOver: false,
       startTurn: DEFAULT_TURN,
-      turn: DEFAULT_TURN, 
+      turn: DEFAULT_TURN,
       gridSize: DEFAULT_GRID_SIZE,
       lastMove: null,
       turnCount: 1,
-      message: '',
-      grid: initGrid()
-    }
+      message: "",
+      grid: initGrid(),
+    };
   }
 
   componentDidMount() {
-    const { turn } = this.state
+    const { turn } = this.state;
 
-    if (turn === 'COMPUTER') {
-      this.calculateComputerMove()
+    if (turn === "COMPUTER") {
+      this.calculateComputerMove();
     }
   }
 
   componentDidUpdate(prevProps, prevState, snapshot) {
-    const { turn, isGameOver} = this.state
+    const { turn, isGameOver } = this.state;
 
-    if (!isGameOver && turn === 'COMPUTER') {
-      this.calculateComputerMove()
+    if (!isGameOver && turn === "COMPUTER") {
+      this.calculateComputerMove();
     }
   }
 
   calculateComputerMove = async () => {
-    const { grid, turnCount } = this.state
+    const { grid, turnCount } = this.state;
     await new Promise((resolve, reject) => {
-      findNextComputerMove(grid, turnCount)
+      findNextComputerMove(grid, turnCount);
       // let the other threads finish
-      setTimeout(resolve, 0)
-    })
+      setTimeout(resolve, 0);
+    });
 
-    this.fillCell(nextMove, 'COMPUTER')
-  }
+    this.fillCell(nextMove, "COMPUTER");
+  };
 
   fillCell = async (cell, player) => {
-    const { grid, turnCount } = this.state
-    const [row, column] = cell
+    const { grid, turnCount } = this.state;
+    const [row, column] = cell;
 
-    const gridCopy = [ ...grid ] // we do this so we don't mutate state itself!
+    const gridCopy = [...grid]; // we do this so we don't mutate state itself!
 
-    gridCopy[row][column] = player
+    gridCopy[row][column] = player;
 
-    const turn = player === 'HOOMAN' ? 'COMPUTER' : 'HOOMAN'
-
+    const turn = player === "HOOMAN" ? "COMPUTER" : "HOOMAN";
 
     await new Promise((resolve, reject) => {
-      checkGameState(grid, cell, turnCount, player)
+      checkGameState(grid, cell, turnCount, player);
       // let the other threads finish
-      setTimeout(resolve, 0)
-    })
+      setTimeout(resolve, 0);
+    });
 
     switch (gameState) {
       case 1:
-        this.endGame('Compooter has won. Press reset to play again!', gridCopy)
-        return
+        this.endGame("Computer has won. Press reset to play again!", gridCopy);
+        return;
       case 2:
-        this.endGame('Congrats Hackerman. Press reset to play again!', gridCopy)
-        return
+        this.endGame(
+          "Congrats Hackerman. Press reset to play again!",
+          gridCopy
+        );
+        return;
       case 3:
-        this.endGame('Nobody won. Press reset to play again!', gridCopy)
-        return
+        this.endGame("Nobody won. Press reset to play again!", gridCopy);
+        return;
     }
 
     this.setState({
       turn,
       grid: gridCopy,
       turnCount: turnCount + 1,
-      lastMove: cell
-    })
-  }
+      lastMove: cell,
+    });
+  };
 
   endGame = (message, grid) => {
     this.setState({
       grid,
       message,
-      isGameOver: true
-    })
-  }
+      isGameOver: true,
+    });
+  };
 
   resetGame = (turn) => {
-    
-    this.setState((prevState, _) => ({
-      message: 'Restarting Game..',
-      isGameOver: false,
-      startTurn: turn || prevState.startTurn,
-      turn: turn || prevState.startTurn || DEFAULT_TURN, 
-      gridSize: DEFAULT_GRID_SIZE,
-      lastMove: null,
-      turnCount: 1,
-      grid: initGrid()
-    }), () => this.setState({ message: ''}))
-  }
-
-
+    this.setState(
+      (prevState, _) => ({
+        message: "Restarting Game..",
+        isGameOver: false,
+        startTurn: turn || prevState.startTurn,
+        turn: turn || prevState.startTurn || DEFAULT_TURN,
+        gridSize: DEFAULT_GRID_SIZE,
+        lastMove: null,
+        turnCount: 1,
+        grid: initGrid(),
+      }),
+      () => this.setState({ message: "" })
+    );
+  };
 
   render() {
-    const { grid, gridSize, turn, isGameOver, message } = this.state
+    const { grid, gridSize, turn, isGameOver, message } = this.state;
 
     return (
-      <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
-        <h1 style={{textAlign: 'center', marginBottom: '5px'}}>Chris's Tic Tac Toe</h1>
-        <h2 style={{textAlign: 'center', marginTop: '5px'}}>{message ? message : (!isGameOver && turn === 'COMPUTER') ? 'Compooter is thinking 🤔' : ' '}</h2>
-        <div style={{ display: 'flex', marginBottom: '10px' }}>
-          <select onChange={(e) => this.resetGame(e.target.value)} style={{ flex: 1, marginRight: '3px'}}>
-            <option value='HOOMAN'>Hooman</option>
-            <option value='COMPUTER'>Compooter</option>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        }}
+      >
+        <h1 style={{ textAlign: "center", marginBottom: "5px" }}>
+          Chris's Tic Tac Toe
+        </h1>
+        <h2 style={{ textAlign: "center", marginTop: "5px" }}>
+          {message
+            ? message
+            : !isGameOver && turn === "COMPUTER"
+            ? "Computer is thinking 🤔"
+            : " "}
+        </h2>
+        <div style={{ display: "flex", marginBottom: "10px" }}>
+          <select
+            onChange={(e) => this.resetGame(e.target.value)}
+            style={{ flex: 1, marginRight: "3px" }}
+          >
+            <option value="HOOMAN">Hooman</option>
+            <option value="COMPUTER">Computer</option>
           </select>
-          <button style={{ flex: 1}} onClick={(e) => this.resetGame()}>Reset</button>
+          <button style={{ flex: 1 }} onClick={(e) => this.resetGame()}>
+            Reset
+          </button>
         </div>
-        <div style={{marginLeft: 'auto', marginRight: 'auto'}}>
-          {
-            grid.map((row, rowIndex) => (
-              <div key={`row-${rowIndex}`} style={{ display: 'flex', flexDirection: 'row', maxWidth: `${gridSize*100 + gridSize - 1}px`,borderBottom: rowIndex < gridSize - 1 ? '1px solid red' : 'none'}}>
-                {
-                  row.map((fill, columnIndex) => (
-                    <Cell
-                      key={`col-${columnIndex}`}
-                      isGameOver={isGameOver}
-                      turn={turn}
-                      fill={fill} // This determines if this cell is empty or not!
-                      cell={[rowIndex, columnIndex]}
-                      gridSize={gridSize}
-                      fillCell={this.fillCell}
-                    />
-                  ))
-                }
-              </div>
-            ))
-          }
+        <div style={{ marginLeft: "auto", marginRight: "auto" }}>
+          {grid.map((row, rowIndex) => (
+            <div
+              key={`row-${rowIndex}`}
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                maxWidth: `${gridSize * 100 + gridSize - 1}px`,
+                borderBottom:
+                  rowIndex < gridSize - 1 ? "1px solid red" : "none",
+              }}
+            >
+              {row.map((fill, columnIndex) => (
+                <Cell
+                  key={`col-${columnIndex}`}
+                  isGameOver={isGameOver}
+                  turn={turn}
+                  fill={fill} // This determines if this cell is empty or not!
+                  cell={[rowIndex, columnIndex]}
+                  gridSize={gridSize}
+                  fillCell={this.fillCell}
+                />
+              ))}
+            </div>
+          ))}
         </div>
       </div>
-    )
+    );
   }
-
-
 }
